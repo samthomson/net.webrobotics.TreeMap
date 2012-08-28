3ea0dc6016e32386f55f48645177b08756765550
Massimo <nve@webrobotics.net>
2012-08-28 19:31:56 +0200

    new features

--- TreeMap.js
+++ TreeMap.js
@@ -57,9 +57,8 @@ net.webrobotics.MapElement.prototype.getLastValue=function(){
 var comparator=function(key1,key2){
     if (key1<key2)
         return -1
-    else if (key1>key2)
-        return 1
-    else return 0;
+    else
+        return (key1>key2)?1:0;
 }
 
 //##########################################################
@@ -87,13 +86,92 @@ var comparator=function(key1,key2){
 
  */
 
-net.webrobotics.TreeMap=function(comparator){
+net.webrobotics.TreeMap=function(comparator,options){
+    this.options=options;
+    this.allowDuplicateKeys=(options)?options.allowDuplicateKeys:true|true;
+    this.suppressDuplicateKeyAlerts=(options)?options.suppressDuplicateKeyAlerts:false|false;
     this.elements=new Array();
     this.comparator=comparator;
 }
 
+net.webrobotics.TreeMap.prototype.containsKey=function(element){
+    if (this.get(element)!=null)
+        return true;
+    return false;
+}
+
+net.webrobotics.TreeMap.prototype.containsValue=function(element){
+    var out=this.toAny(function(current){
+        return (element==current)?true:null;
+    });
+    return (out!=null)?true:false;
+}
+
+net.webrobotics.TreeMap.prototype.toAny=function(callback){
+    var out;
+    var values=this.toArray();
+    for (var n=0;n<values.length;n++){
+        out=callback(values[n]);
+        if (out!=null)
+            return out;
+    }
+}
+
+net.webrobotics.TreeMap.prototype.forEach=function(callback){
+    var values=this.toArray();
+    for (var n=0;n<values.length;n++)
+        callback(values[n]);
+}
+
+// filteredFunction takes an element and returns a boolean value that establishes
+// if the element is part of the sub-treeMap
+net.webrobotics.TreeMap.prototype.getFilteredSubTreeMapByValue=function(filterFunction){
+    var newTreeMap=new net.webrobotics.TreeMap(this.comparator,this.options);
+    var tmpMapElement,n,i,value;
+    for (n=0;n<this.elements.length;n++){
+        tmpMapElement=this.elements[n];
+        for (i=0;i<tmpMapElement.getValues().length;i++){
+            value=tmpMapElement.getValues()[i];
+            if (filterFunction(value)==true){
+                newTreeMap.put(tmpMapElement.getKey(),value);
+            }
+        }
+    }
+    return newTreeMap;
+}
+
+// filteredFunction takes a key and returns a boolean value that establishes
+// if the values with this key are part of the sub-treeMap
+net.webrobotics.TreeMap.prototype.getFilteredSubTreeMapByKey=function(filterFunction){
+    var newTreeMap=new net.webrobotics.TreeMap(this.comparator,this.options);
+    var tmpMapElement,n;
+    for (n=0;n<this.elements.length;n++){
+        tmpMapElement=this.elements[n];
+        if (filterFunction(tmpMapElement.getKey())==true){
+            newTreeMap.elements.push(tmpMapElement);
+        }
+    }
+    return newTreeMap;
+}
+
+net.webrobotics.TreeMap.prototype.getSubTreeMap=function(keyStart,keyStop){
+    var newTreeMap;
+    var start=this.positionOf(keyStart);
+    var stop=this.positionOf(keyStop);
+
+    if (!this.elements[stop] || this.comparator(this.elements[stop].getKey(),keyStop)!=0){
+        stop--;
+    }
+
+    if (start<=stop){
+        newTreeMap=new net.webrobotics.TreeMap(this.comparator,this.options);
+        newTreeMap.elements=this.elements.slice(start,stop+1);
+    }
+    return newTreeMap;
+}
+
 net.webrobotics.TreeMap.prototype.size=function(duplicates){
-    if (duplicates){
+    if (duplicates && this.allowDuplicateKeys){
         var out=0;
         for (var n=0;n<this.elements.length;n++){
             out+=this.elements[n].getValues().length
@@ -104,16 +182,24 @@ net.webrobotics.TreeMap.prototype.size=function(duplicates){
 }
 
 net.webrobotics.TreeMap.prototype.last=function(){
-    return this.elements[this.size()-1].getValues();
+    var elements=this.elements[this.size()-1].getValues();
+    if (this.allowDuplicateKeys)
+        return elements;
+    else
+        return elements[0];
 }
 
 net.webrobotics.TreeMap.prototype.first=function(){
-    return this.elements[0].getValues();
+    var elements=this.elements[0].getValues();
+    return (this.allowDuplicateKeys)?elements:elements[0];
 }
 
 net.webrobotics.TreeMap.prototype.at=function(at){
-    if (this.elements[at])
-        return this.elements[at].getValues();
+    var elements;
+    if (this.elements[at]){
+        elements=this.elements[at].getValues();
+        return (this.allowDuplicateKeys)?elements:elements[0];
+    }
     return null;
 }
 
@@ -130,20 +216,18 @@ net.webrobotics.TreeMap.prototype.put=function(key,value){
 
     var comparation=this.comparator(this.elements[this.elements.length-1].getKey(),key);
     if (comparation<=0){
-        if (comparation==0){
-            position=this.elements.length-1;
-        }else{
-            position=this.elements.length;
-        }
-    }else if (this.comparator(this.elements[0].getKey(),key)>=0){
-        position=0;
+        position= (comparation==0)?(this.elements.length-1):(this.elements.length);
     }else{
-        position=this.positionOf(key);
+        position=(this.comparator(this.elements[0].getKey(),key)>=0)?0:this.positionOf(key);
     }
+
 
     cur_element=this.elements[position];
     if (cur_element!=null && this.comparator(cur_element.getKey(),key)==0){
-        cur_element.addValue(value);
+        if (this.allowDuplicateKeys)
+            cur_element.addValue(value);
+        if (!this.suppressDuplicateKeyAlerts)
+            console.log("Duplicate key: "+key.toString());
     }else{
         var tmp=new Array();
         tmp=tmp.concat(this.elements.slice(0,position));
@@ -164,28 +248,20 @@ net.webrobotics.TreeMap.prototype.positionOf=function(key){
             return half;
         }else{
             if (size==1){
-                if (comparation==-1){
-                    return start+1;
-                }else{
-                    return start;
-                }
+                return (comparation==-1)?start+1:start;
             }else{
-                if (comparation==-1){
-                    return this.positionOfTR(key,elements,half,stop);
-                }else{
-                    return this.positionOfTR(key,elements,start,half-1);
-                }
+                return (comparation==-1)?this.positionOfTR(key,elements,half,stop):this.positionOfTR(key,elements,start,half-1);
             }
         }
     }
-
     return this.positionOfTR(key,this.elements,0,this.elements.length-1);
 }
 
 net.webrobotics.TreeMap.prototype.toArray=function(){
     var out=new Array();
-    for(var n=0;n<this.elements.length;n++){
-        var el=this.elements[n];
+    var el,n;
+    for(n=0;n<this.elements.length;n++){
+        el=this.elements[n];
         out=out.concat(el.getValues());
     }
     return out;
@@ -194,10 +270,12 @@ net.webrobotics.TreeMap.prototype.toArray=function(){
 
 net.webrobotics.TreeMap.prototype.toString=function(){
     var out="";
+    var el, n,i;
+    var vals="";
     for(var n=0;n<this.elements.length;n++){
-        var el=this.elements[n];
-        var vals="";
-        for (var i=0;i<el.getValues().length;i++){
+        el=this.elements[n];
+        vals="";
+        for (i=0;i<el.getValues().length;i++){
             vals+=el.getValues()[i]+",";
         }
         out+="<"+el.getKey()+","+vals+">; "
@@ -230,10 +308,15 @@ net.webrobotics.TreeMap.prototype.get=function(key){
 }
 
 net.webrobotics.TreeMap.prototype.nearest=function(key,after){
+    var elements;
     var position=this.positionOf(key);
     var element=this.elements[position];
-    if((element != null && this.comparator(element.getKey(),key)==0) || after) {
-        return element.getValues();
+    if(after || (element!=null && this.comparator(element.getKey(),key)==0)) {
+        if (element!=null){
+            elements=element.getValues();
+            return (this.allowDuplicateKeys)?elements:elements[0];
+        }
+        return null;
     }
     return this.at(position-1);
 }
